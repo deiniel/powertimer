152a0af977917da002b942462ed074221998a3de
deiniel <Dani@gort.local>
2016-04-19 19:52:47 +0200

    Update powertime.py with new methods for loop control, reset and change some public variables to private.
    Now the PWTimer class can be looped and the update of the reset() method is able to restart the loop values if needed.
    Testing will be performed following the ATDD method with the robotframework -WIP, the testing environment is done in feature/testing_with_robotframework branch-

--- powertimer.py
+++ powertimer.py
@@ -14,41 +14,42 @@ class PWTimer(_Timer):      # PowerTimer class
         self.stop() method will never be called and the user has to take care of that)
     time_unit: the unit of time (secods, minutes) the interal is applied. By defaut 'seconds'
     """
-    def __init__(self, interval, function, loop = 1, time_unit='seconds'):
-        self.time_unit = time_unit
-        if self.time_unit == 'seconds':
-            self.multiplier = 1
-        elif self.time_unit == 'minutes':
-            self.multiplier = 60
-        elif self.time_unit == 'hours':
-            self.multiplier = 3600
-        elif self.time_unit == 'light_years':
+    def __init__(self, interval, function, loop=1, time_unit='seconds'):
+        self.__time_unit = time_unit
+        if self.__time_unit == 'seconds':
+            self.__multiplier = 1
+        elif self.__time_unit == 'minutes':
+            self.__multiplier = 60
+        elif self.__time_unit == 'hours':
+            self.__multiplier = 3600
+        elif self.__time_unit == 'light_years':
             pass        # ok, you implement this one :P
         self.paused = Event()
-        self.interval = self.multiplier * interval
+        self.__interval = self.__multiplier * interval
         self.function = function
         self.loop = loop
         self.loopEvent = Event()
         if self.loop == 0:
-            self.loopAlways = True
+            self.__loopAlways = True
         elif self.loop > 0:
-            self.loopAlways = False
+            self.__loopAlways = False
         self.__actual_count = 0
-        self.remaining_interval = self.interval - self.__actual_count
-        super(PWTimer, self).__init__(self.interval, self.function, args=[], kwargs={})
+        self.remaining_interval = self.__interval - self.__actual_count
+        super(PWTimer, self).__init__(self.__interval, self.function, args=[], kwargs={})
 
     def run(self):          # overwrites the original run() from _Timer so PWTimer can return the current timer counter
         while not self.loopEvent.is_set():
+            self.__update_loop()
             while not self.finished.is_set():
                 while self.paused.is_set():
                     pass        #TODO: more elegant way to wait when the timer is paused
+                print self.remaining_interval
                 self.__actual_count += 1
                 self.finished.wait(1)
-                self.remaining_interval = self.interval - self.__actual_count
-                if self.interval - self.__actual_count == 0:
+                self.remaining_interval = self.__interval - self.__actual_count
+                if self.__interval - self.__actual_count == 0:
                     self.finished.set()
                     self.function(*self.args, **self.kwargs)
-            self.update_loop()
 
     def stop(self):         # does the same as cancel() in the _Timer class but the name is more natural
         self.finished.set()
@@ -59,37 +60,45 @@ class PWTimer(_Timer):      # PowerTimer class
     def resume(self):       # resumes a paused timer
         self.paused.clear()
 
-    def update_loop(self):
-        if not self.loopAlways:
+    def __update_loop(self):
+        if not self.__loopAlways:
             if self.loop > 0:
                 self.loop -= 1
+                self.__reset_counters()
+                self.finished.clear()
+                self.paused.clear()
             elif self.loop == 0:
                 self.loopEvent.set()
+        elif self.__loopAlways:
+            self.__reset_counters()
+            self.finished.clear()
+            self.paused.clear()
 
-    def restart(self, new_value=None):      # restarts the timer at "runtime" or when is already finished
-        if new_value is not None:
-            self.interval = self.multiplier * new_value
+    def __reset_counters(self):
+        self.__actual_count = 0
+        self.remaining_interval = self.__interval - self.__actual_count
+
+
+    def restart(self, new_interval_value=None, new_loop_value=None):      # restarts the timer at "runtime" or when is already finished
+        if new_interval_value is not None:
+            self.__interval = self.__multiplier * new_interval_value
+
+        if new_loop_value is not None:
+            self.loop = new_loop_value
+            self.__update_loop()
 
         if self.remaining_interval > 0:
             self.paused.set()
-            self.__actual_count = 0
-            self.remaining_interval = self.interval - self.__actual_count
+            self.__reset_counters()
             self.paused.clear()
 
         elif self.remaining_interval == 0:
-            self.__actual_count = 0
-            self.remaining_interval = self.interval - self.__actual_count
+            self.__reset_counters()
             self.finished.clear()
             self.paused.clear()
-            super(PWTimer, self).__init__(self.interval, self.function, args=[], kwargs={})
+            super(PWTimer, self).__init__(self.__interval, self.function, args=[], kwargs={})
             self.start()
 
 
 class PWCounter(object):    # PowerCounter class
     pass
-
-def dummy():
-    print 'hola!'
-
-my_timer = PWTimer(5, dummy, loop=0)
-my_timer.start()
